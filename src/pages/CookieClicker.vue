2a700fdaba85181fd6668e914de162a0498ede37
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-10-27 15:59:21 +0200

    Add cookie clicker

---
+++ src/pages/CookieClicker.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+
+let cookies = ref(0);
+
+</script>
+<template>
+    <div class="columns">
+        <div class="column has-background-primary has-text-centered">
+            <h1>{{ cookies }} cookies!</h1>
+            <div @click="cookies++">
+                <img src="https://png.pngtree.com/png-vector/20250425/ourmid/pngtree-chocolate-chip-cookie-icon-white-background-png-image_16100260.png">
+            </div>
+        </div>
+        <div class="column has-background-warning">
+            test
+        </div>
+        <div class="column has-background-info">
+            test
+        </div>
+    </div>
+</template>
