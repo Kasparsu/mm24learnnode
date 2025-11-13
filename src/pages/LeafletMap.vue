d249ce4024e2b86fad9c96c959f3aba7dc2793f6
trentonpro <trent.schotter@gmail.com>
2025-11-13 14:14:47 +0200

    Added Viru Mcdonalds to leaflet

--- src/pages/LeafletMap.vue
+++ src/pages/LeafletMap.vue
@@ -10,11 +10,24 @@ let coords = ref({
 </script>
 
 <template>
-    <div>
-        <input v-model="zoom" type="range" max="19" min="1" step="1">
-        {{ zoom }}   
-        <button class="button is-primary" @click="coords={lat: 59.41641, lng:  24.79871}">Go to Airport</button>
-    </div>
-    <Leaflet :lat="coords.lat" :lng="coords.lng" :zoom="zoom"></Leaflet>
-    <Leaflet :lat="24.74344" :lng="59.42690" :zoom="5"></Leaflet>
+ <div>
+    <input v-model="zoom" type="range" max="19" min="1" step="1" />
+    {{ zoom }}
+    <button
+      class="button is-primary"
+      @click="coords = { lat: 59.41641, lng: 24.79871 }"
+    >
+      Go to Airport
+    </button>
+    <button
+      class="button is-primary"
+      @click="coords = { lat: 59.43684, lng: 24.75198 }"
+    >
+      Go to Viru McDonald's
+    </button>
+  </div>
+  <Leaflet :lat="coords.lat" :lng="coords.lng" :zoom="zoom"></Leaflet>
+  <Leaflet :lat="59.43648031071515" :lng="24.74987808706019" :zoom="5"></Leaflet>
+    
+
 </template>
