11c8915b38173b68bef1bef8a0b4589844f6b4a6
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-11-20 15:48:48 +0200

    Add inquirer

--- bin/terminal.js
+++ bin/terminal.js
@@ -7,3 +7,21 @@ import chalk from "chalk";
 write(chalk.red.bgYellow.strikethrough('DANGER!!'));
 write(chalk.bgAnsi256(67)('COLOR'));
 write(chalk.bgRgb(255,0,0)('COLOR'));
+
+import inquirer from "inquirer";
+
+let answers = await inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+    },
+    {
+        type: 'number',
+        name: 'age',
+        message: 'What is your age?',
+    },
+]);
+
+console.log(`Hello ${answers.name}!!!!`);
+console.log(`You are ${answers.age} years old!`);
