242a21ea3f68942c6c79ec5e02e1edd5a8dba39c
Tristan Kruuk <tristanhans.kruuk@tptlive.ee>
2025-11-26 20:32:47 +0200

    Add upgrades

--- src/pages/CookieClicker.vue
+++ src/pages/CookieClicker.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue';
 import BuildingButton from '../components/BuildingButton.vue';
+import UpgradeButton from '../components/UpgradeButton.vue';
 
 let cookies = ref(0);
 let cps = ref(0);
@@ -10,19 +11,34 @@ setInterval(() => {
 }, 1000);
 
 let buildings = ref([
-    {name: 'Cursor', icon: '👆🏻', cps: 0.1, price: 15, count: 0},
-    {name: 'Grandma',icon: '👵🏻', cps: 1, price: 100, count: 0},
+    { name: 'Cursor', icon: '👆🏻', cps: 0.1, price: 15, count: 0 },
+    { name: 'Grandma', icon: '👵🏻', cps: 1, price: 100, count: 0 },
 ]);
 
 function buyBuilding(building) {
-    if(cookies.value >= building.price) {
+    if (cookies.value >= building.price) {
         cookies.value -= building.price;
         cps.value += building.cps;
         building.count++;
     }
 }
 
+let upgrades = ref([
+    { name: 'Double CPS 1', icon: '🌧️', price: 1000, bought: false },
+    { name: 'Double CPS 2', icon: '⛈️', price: 10000, bought: false },
+    { name: 'Double CPS 3', icon: '⚡', price: 100000, bought: false },
+    { name: 'Double CPS 4', icon: '☀️', price: 1000000, bought: false },
+    { name: 'Double CPS 4', icon: '🌕', price: 10000000, bought: false },
+    { name: 'Double CPS 4', icon: '🌈', price: 100000000, bought: false },
+]);
 
+function buyUpgrade(upgrade) {
+    if (cookies.value >= upgrade.price && !upgrade.bought) {
+        cookies.value -= upgrade.price;
+        cps.value *= 2;
+        upgrade.bought = true;
+    }
+}
 </script>
 <template>
     <div class="columns">
@@ -38,7 +54,28 @@ function buyBuilding(building) {
             test
         </div>
         <div class="column has-background-info">
-           <BuildingButton v-for="building in buildings" :building="building" :disabled="cookies<building.price" @click="buyBuilding(building)"></BuildingButton>
+            <div class="upgradecontainer">
+                <UpgradeButton v-for="u in upgrades.filter(up => !up.bought)" :key="u.name" :upgrade="u"
+                    :disabled="cookies < u.price" @click="buyUpgrade(u)" />
+            </div>
+            <BuildingButton v-for="building in buildings" :building="building" :disabled="cookies < building.price"
+                @click="buyBuilding(building)"></BuildingButton>
         </div>
     </div>
 </template>
+
+<style>
+.column{
+    width: calc(100vw/3);
+}
+
+.upgradecontainer{
+    overflow-x: auto;
+    display: flex;
+    gap: 12px;
+}
+
+.upgradecontainer::-webkit-scrollbar {
+  display: none;
+}
+</style>
