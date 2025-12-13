6eaa1115debe416cb3bbcc06a455305ca2403bce
stweeeezy <stenlohvart402@gmail.com>
2025-12-13 19:40:54 +0200

    Add 6 different boxen examples

--- bin/boxes.js
+++ bin/boxes.js
@@ -1,8 +1,47 @@
 import boxen from 'boxen';
 import chalk from 'chalk';
 
-console.log(boxen(chalk.red('unicorn'), {padding: 1}));
+// 1
+console.log(
+  boxen(chalk.red('UNICORN'), { padding: 1 })
+);
 
-console.log(boxen('unicorn', {padding: 1, margin: 1, borderStyle: 'double'}));
+// 2
+console.log(
+  boxen(
+    chalk.green('UNICORN'),
+    { padding: 1, margin: 1, borderStyle: 'double' }
+  )
+);
 
-console.log(boxen('unicorns love rainbows', {title: 'magical', titleAlignment: 'center'}));
+// 3
+console.log(
+  boxen(
+    chalk.blue('UNICORNS LOVE RAINBOWS'),
+    { title: 'MAGICAL', titleAlignment: 'center' }
+  )
+);
+
+// 4
+console.log(
+  boxen(
+    chalk.yellow.bold('HELLO'),
+    { borderStyle: 'round', padding: 1 }
+  )
+);
+
+// 5
+console.log(
+  boxen(
+    chalk.magenta('FANTASY'),
+    { borderStyle: 'bold', padding: 2 }
+  )
+);
+
+// 6
+console.log(
+  boxen(
+    chalk.inverse('THE END'),
+    { padding: { top: 1, bottom: 1, left: 4, right: 4 } }
+  )
+);
